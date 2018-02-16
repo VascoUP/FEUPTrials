3c4cf520baf84a6e9a0fffe67bcd5b2e3b718034
VascoUP <vasco_mag_pereira@hotmail.com>
2018-02-16 20:54:22 +0000

    Implemented wheel rotation

---
+++ FEUP Trials/Assets/FEUPTrials/Assets/scripts/BikeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BikeController : MonoBehaviour {
+    [SerializeField]
+    private GameObject _frontWheel;
+
+    [SerializeField]
+    private GameObject _backWheel;
+
+    public const float MAX_SPEED = 500;
+    public float rotationSpeed;    
+
+	void Update () {
+        if(Input.GetKey(KeyCode.W))
+        {
+            if (rotationSpeed + 5 > MAX_SPEED)
+                rotationSpeed = MAX_SPEED;
+            else
+                rotationSpeed += 5f;
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            if (Mathf.Abs(rotationSpeed) + 5 > MAX_SPEED)
+                rotationSpeed = -MAX_SPEED;
+            else
+                rotationSpeed -= 5f;
+        }
+        else if(rotationSpeed != 0)
+        {
+            if(Mathf.Abs(rotationSpeed) - 5f < 0)
+                rotationSpeed = 0;
+            else if (rotationSpeed > 0)
+                rotationSpeed -= 5f;
+            else if(rotationSpeed < 0)
+                rotationSpeed += 5f;
+        }
+
+        _frontWheel.transform.Rotate(Vector3.back, rotationSpeed * Time.deltaTime);
+        _backWheel.transform.Rotate(Vector3.back, rotationSpeed * Time.deltaTime);
+    }
+}
