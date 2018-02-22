0eaaad0b4bb5742125cbfea0b303e3044e5becbf
VascoUP <vasco_mag_pereira@hotmail.com>
2018-02-22 18:17:41 +0000

    Implemented bike basic movements

--- FEUP Trials/Assets/FEUPTrials/Assets/scripts/BikeController.cs
+++ FEUP Trials/Assets/FEUPTrials/Assets/scripts/BikeController.cs
@@ -3,19 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BikeController : MonoBehaviour {
-    [SerializeField]
+    private Rigidbody2D _bikeRB;
     private WheelJoint2D _frontWheel;
-    [SerializeField]
+    private Rigidbody2D _frontWheelRB;
     private WheelJoint2D _backWheel;
+    private Rigidbody2D _backWheelRB;
 
-    public float maxSpeed = 1000;
+    public float maxForce = 500;
     // Acceleration (in force) per second
     public float forceAcceleration = 500;
     // Motor force
     public float motorForce;
+    // Velocity at which the bike needs to be to brake
+    public float velocityThereshold;
+
+    // Angular drag value for when the bike is braking
+    public float brakeForce;
+
+    public float angularDrag;
+
+    private bool _braking = false;
 
     private void Start()
     {
+        _bikeRB = GetComponent<Rigidbody2D>();
+
         WheelJoint2D[] wheelJoints = GetComponents<WheelJoint2D>();
         foreach(WheelJoint2D w in wheelJoints)
         {
@@ -27,9 +39,56 @@ public class BikeController : MonoBehaviour {
 
         if (_backWheel == null || _frontWheel == null)
             Debug.LogError("Null wheel");
+
+        _frontWheelRB = _frontWheel.GetComponent<Rigidbody2D>();
+        _frontWheelRB.angularDrag = angularDrag;
+        _backWheelRB = _backWheel.GetComponent<Rigidbody2D>();
+        _backWheelRB.angularDrag = angularDrag;
+
+        _backWheel.useMotor = false;
+    }
+    
+    private float DirectionalVelocity()
+    {
+        float velocity = Mathf.Cos(transform.rotation.z) * _bikeRB.velocity.x + Mathf.Sin(transform.rotation.z) * _bikeRB.velocity.y;
+        return velocity;
     }
 
-    void Update () {
+    private void Brake()
+    {
+        _braking = true;
+
+        _backWheel.useMotor = false;
+        motorForce = 0;
+        
+        _frontWheelRB.drag = brakeForce;
+        _backWheelRB.drag = brakeForce;
+    }
+
+    private void StopBrake()
+    {
+        _braking = false;
+
+        _backWheel.useMotor = true;
+
+        _frontWheelRB.drag = angularDrag;
+        _backWheelRB.drag = angularDrag;
+    }
+
+    private void UpdateMotorSpeed(float speed)
+    {
+        if (!_backWheel.useMotor)
+            return;
+
+        _frontWheelRB.drag = angularDrag;
+        _backWheelRB.drag = angularDrag;
+
+        JointMotor2D motor = _backWheel.motor;
+        motor.motorSpeed = -speed;
+        _backWheel.motor = motor;
+    }
+
+    void Update() {
         UpdateMotorForce();
         //UpdateRotation();
     }
@@ -38,29 +97,46 @@ public class BikeController : MonoBehaviour {
     {
         if (Input.GetKey(KeyCode.W))
         {
-            // Move forward OR brake
+            _backWheel.useMotor = true;
+
+            if (_braking)
+                StopBrake();
+
+            // Move forward
             motorForce += forceAcceleration * Time.deltaTime;
+
+            if (motorForce > maxForce)
+                motorForce = maxForce;
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            // Move backwards OR brake
-            motorForce -= forceAcceleration * Time.deltaTime;
-        }
-        /*else
-        {
-            float signal = (motorForce / Mathf.Abs(motorForce));
-            float force = Mathf.Abs(motorForce) - forceAcceleration;
-            if((force >= 0 && motorForce < 0) || (force <= 0 && motorForce > 0))
+            _backWheel.useMotor = true;
+
+            //Brake
+            if (DirectionalVelocity() > velocityThereshold)
             {
-                force = 0;
+                if (!_braking)
+                    Brake();
             }
             else
             {
-                motorForce = signal * force;
-            }
-        }*/
+                if (_braking)
+                    StopBrake();
 
-        SetMotorSpeed(motorForce);
+                // Move backwards
+                motorForce = -100;
+            }
+        }
+        else
+        {
+            if (_braking)
+                StopBrake();
+
+            _backWheel.useMotor = false;
+            motorForce = 0;
+        }
+
+        UpdateMotorSpeed(motorForce);
     }
 
     void UpdateRotation()
@@ -74,13 +150,5 @@ public class BikeController : MonoBehaviour {
             // Rotate with negative angle
         }
 
-    }
-
-    // Negative speed should move the player backwards, but moves forward
-    void SetMotorSpeed(float speed)
-    {
-        JointMotor2D motor = _backWheel.motor;
-        motor.motorSpeed = speed;
-        _backWheel.motor = motor;
     }
 }
---
+++ FEUP Trials/Assets/FEUPTrials/Assets/scripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    [SerializeField]
+    private GameObject _playerObject;
+
+    public Vector3 offset;
+    	
+	void LateUpdate () {
+        transform.position = _playerObject.transform.position + offset;
+    }
+}
